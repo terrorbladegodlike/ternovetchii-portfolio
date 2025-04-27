b28fa50793719b96bdda00b5b710c414ad4d47bd
terrorbladegodlike <ternovetchii.2002@mail.ru>
2025-04-27 12:11:47 +0300

    fixed foto

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
             <div className='col-span-1 xl:row-span-3'>
                <div className='grid-container'>
                   <img
-                     src='assets/grid2.png'
+                     src='assets/grid7.png'
                      alt='grid-2'
                      className='w-full sm:h-[276px] h-fit object-contain'
                   />
